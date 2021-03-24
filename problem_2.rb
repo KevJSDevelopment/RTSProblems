62a8b9b2451c5109fec3614a1afa24bb175467ef
KevJSDevelopment <Kshhn12@gmail.com>
2021-03-24 15:48:44 -0400

    Answers completed

--- problem_2.rb
+++ problem_2.rb
@@ -1,5 +1,6 @@
-# #2  Rotate the characters in a string by a given input and have the overflow appear at the beginning, 
-#e.g. “MyString” rotated by 2 is “ngMyStri”.
+# Q#2:  
+#   Rotate the characters in a string by a given input and have the overflow appear at the beginning, 
+#   e.g. “MyString” rotated by 2 is “ngMyStri”.
 
 def rotate_string_by_input(string, input)
 
